6e5bc80994adb9049f0adc232d1b33bb1a4bc627
Roman Romanenko <romu4444@gmail.com>
2018-02-18 13:06:43 +0300

    - New header background

--- routes/deposit.js
+++ routes/deposit.js
@@ -2,10 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/deposit', function(req, res, next) {
-  res.render('deposit', {
-      title: 'Вклады',
-      text: 'Выбирайте наши депозиты!'
-    });
+  res.render('deposit', { title: 'Вклады' });
 });
 
 module.exports = router;
