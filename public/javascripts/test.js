43f6e956fe72c7e8367a5b64c3319c3b9bc08ac9
Alex Stuhalsky <styxalski98@gmail.com>
2018-03-26 14:59:38 +0300

    added temporary js file

---
+++ public/javascripts/test.js
@@ -0,0 +1,25 @@
+$(document).foundation()
+
+function el(id) {
+  return document.getElementById(id);
+}
+
+var planets_info = [
+  { name: 'Сатурн', num: '17'},
+  { name: 'Юпитер', num: '16'},
+  { name: 'Уран', num: '14'},
+  { name: 'Марс', num: '2'},
+  { name: 'Нептун', num: '2'},
+  { name: 'Земля', num: '1'},
+  { name: 'Плутон', num: '1'}
+]
+
+function satellite_click(satellite_num, satellite_res) {
+  var res = "fuck";
+  var num = parseInt(el(satellite_num).value);
+  // foreach(pl in planets_info) {
+  //   if (pl.num >= num)
+  //     res += pl.name + " ";
+  // }
+  el(satellite_res).value = res;
+}
